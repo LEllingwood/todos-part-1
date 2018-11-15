ab5ed37b916bfb3228ac22c40a3e26bce4c55471
Lea Ellingwood <leaellingwood@hotmail.com>
2018-11-15 13:44:08 -0500

    Updates

---
+++ precache-manifest.6f2bea0dd2ca2815d99c062a104c5945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ced8597f93d1e0b2f2c",
+    "url": "/todos-part-1/static/js/runtime~main.6ced8597.js"
+  },
+  {
+    "revision": "cf7e1aa430739c674426",
+    "url": "/todos-part-1/static/js/main.cf7e1aa4.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/todos-part-1/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "cf7e1aa430739c674426",
+    "url": "/todos-part-1/static/css/main.c194320d.chunk.css"
+  },
+  {
+    "revision": "ad0ec7b90154bae23ec984c75504a39e",
+    "url": "/todos-part-1/index.html"
+  }
+];
